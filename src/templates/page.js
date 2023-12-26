ccbe254e82fe9db36b44b863932c1afe6d05c97e
rishulpu1 <rishulpu1@gmail.com>
2023-12-26 20:00:38 +0530

    render a custom component for perticular wp block

--- src/templates/page.js
+++ src/templates/page.js
@@ -2,10 +2,38 @@ import React from "react";
 import {
   BlockRenderer,
   BlockRendererProvider,
+  getStyles,
+  getClasses,
 } from "@webdeveducation/wp-block-tools";
 
 const Page = (props) => {
-  return <BlockRendererProvider allBlocks={props.pageContext.blocks} />;
+  return (
+    <BlockRendererProvider
+      allBlocks={props.pageContext.blocks}
+      renderComponent={(block) => {
+        switch (block.name) {
+          case "core/media-text": {
+            const content = <BlockRenderer blocks={block.innerBlocks} />;
+            return (
+              <div
+                key={block.id}
+                style={getStyles(block)}
+                className={getClasses(block)}
+              >
+                {block.attributes.mediaPosition === "right" && (
+                  <div>{content}</div>
+                )}
+                <div>asd</div>
+                {block.attributes.mediaPosition !== "right" && (
+                  <div>{content}</div>
+                )}
+              </div>
+            );
+          }
+        }
+      }}
+    />
+  );
 };
 
 export default Page;
